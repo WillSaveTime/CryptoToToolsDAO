0850116540f531dcc888496e8c003428da2c38f7
WIllSaveTime <cooker0910@gmail.com>
2023-01-27 11:29:15 -0300

    verify email

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -7,7 +7,7 @@ export default {
   auth: {
     root: () => '/auth',
     logIn: () => '/auth/log_in',
-    verify: () => '/auth/verify',
+    verifyEmail: () => '/auth/verify',
     logInWithNumio: () => '/auth/numio',
     signUp: () => '/auth/sign_up',
   },
